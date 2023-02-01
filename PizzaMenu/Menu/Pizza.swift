c3316fadcdb88e777fc1320b56cb5ab6be58de39
mkBajaj253 <maheshm253@gmail.com>
2023-02-01 05:22:10 -0500

    Initial Setup

---
+++ PizzaMenu/Menu/Pizza.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+struct Pizza {
+    let name: String
+ }
---
+++ PizzaMenu/Menu/Topping.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct Topping {
+    let name: String
+    let isVegan: Bool
+    let allergens: [String]
+}
+
